150cb6b19dc4eb968f2f029c998c1d6c7a2fec06
David Pryce <david.pryce@wandera.com>
2020-03-02 15:54:19 +0000

    Updates for name change and test pypi deployment

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     author_email="david.pryce@wandera.com",
     description="A Python client and wrapper around the 1Password CLI.",
     long_description=readme(),
+    long_description_content_type='text/markdown',
     install_requires=[
         "wget>=3.2",
         "pyyaml>=5.1.2",
@@ -25,7 +26,7 @@ setup(
     url="https://github.com/dtpryce/1password",
     classifiers=["Programming Language :: Python :: 3",
                  "License :: OSI Approved :: MIT License",
-                 "Operating System :: MacOS :: MacOS", ],
+                 "Operating System :: MacOS :: MacOS X" ],
     packages=["onepassword"],
     tests_require=["nose", "mock"],
     test_suite="nose.collector",
